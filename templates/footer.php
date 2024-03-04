974743cf699e6b849007e5e10033119587dad126
Rafael Botelho Cabral <118222979+rafaelbcabral@users.noreply.github.com>
2024-03-04 18:26:31 -0300

    first commit

---
+++ templates/footer.php
@@ -0,0 +1,23 @@
+<footer id="footer">
+  <div id="social-container">
+    <ul>
+      <li>
+        <a href="https://wa.me/5522988103858"><i class="fab fa-whatsapp"></i></a>
+      </li>
+      <li>
+        <a href="https://www.instagram.com/cabralrafael23/"><i class="fab fa-instagram"></i></a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/rafael-b-cabral/"><i class="fab fa-linkedin"></i></a>
+      </li>
+    </ul>
+  </div>
+  <br>
+  <p>&copy; Portfólio, <br> <a href="https://github.com/rafaelbcabral" type="bold" class="copy"> </i> 📫 Rafael B. Cabral. <br></a> 
+
+</footer>
+<script src=" https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer">
+</script>
+</body>
+
+</html>
---
+++ templates/header.php
@@ -0,0 +1,36 @@
+<!-- Helpers e  Posts -->
+<?php
+include_once("helpers/url.php");
+include_once("data/posts.php");
+include_once("data/categorias.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Programming</title>
+  <!-- Google fonts -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <!-- CSS Aplicação -->
+  <link rel="short icon" href="img/logo.svg" />
+  <link rel="stylesheet" href="<?= $BASE_URL ?>/css/styles.css">
+</head>
+
+<body>
+  <header>
+    <a href="<?= $BASE_URL ?>" id="logo">
+      <img src="<?= $BASE_URL ?>/img/logo.svg" alt="Blog Codar">
+    </a>
+    <nav>
+      <ul id="navbar">
+        <li><a href="index.php" class="nav-link">Home</a></li>
+        <li><a href="https://www.w3schools.com/" class="nav-link">Categorias</a></li>
+        <li><a href="https://www.linkedin.com/in/rafael-b-cabral/" class="nav-link">Sobre</a></li>
+        <li><a href="contact.php" class="nav-link">Contato</a></li>
+      </ul>
+    </nav>
+  </header>
